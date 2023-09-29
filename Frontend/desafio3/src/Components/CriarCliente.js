8a0c64ad007ffc2bc389f07090142a60c1a2073b
Felipe Santa Cruz Simão <felipesantacruz6@gmail.com>
2023-09-29 20:28:44 -0300

    fix(CriarCliente)arrumando tratamento de cnpj ao ser gravado

--- Frontend/desafio3/src/Components/CriarCliente.js
+++ Frontend/desafio3/src/Components/CriarCliente.js
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import InputMask from 'react-input-mask';
 import validator from 'validator';
 import axios from 'axios';
-import { validate } from 'cnpj';
-
 
 function CriarCliente() {
   const [formData, setFormData] = useState({
@@ -18,41 +16,83 @@ function CriarCliente() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+
     setFormData({
       ...formData,
       [name]: value,
     });
   };
 
+  const isCnpjValid = (cnpj) => {
+    // Remove qualquer caracter não numérico do CNPJ
+    cnpj = cnpj.replace(/\D/g, '');
+
+    if (cnpj.length !== 14) return false;
+
+    // Verifica se todos os dígitos são iguais (situação inválida)
+    if (/^(\d)\1+$/.test(cnpj)) return false;
+
+    let tamanho = cnpj.length - 2;
+    let numeros = cnpj.substring(0, tamanho);
+    const digitos = cnpj.substring(tamanho);
+    let soma = 0;
+    let pos = tamanho - 7;
+
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2) pos = 9;
+    }
+
+    let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+    if (resultado.toString() !== digitos.charAt(0)) return false;
+
+    tamanho += 1;
+    numeros = cnpj.substring(0, tamanho);
+    soma = 0;
+    pos = tamanho - 7;
+
+    for (let i = tamanho; i >= 1; i--) {
+      soma += numeros.charAt(tamanho - i) * pos--;
+      if (pos < 2) pos = 9;
+    }
+
+    resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+    return resultado.toString() === digitos.charAt(1);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
- 
+    const { cnpj, mcc, emailContato } = formData;
 
+    // Remove barras e traços do CNPJ antes de enviar
+    const cnpjLimpo = cnpj.replace(/[^\d]+/g, '');
 
-
-    if (!validate(formData.cnpj)) {
+    if (!isCnpjValid(cnpjLimpo)) {
       alert('CNPJ inválido');
       return;
     }
 
-    if (!validator.isNumeric(formData.mcc) || formData.mcc.length !== 4) {
+    if (!validator.isNumeric(mcc) || mcc.length !== 4) {
       alert('MCC deve conter exatamente 4 dígitos numéricos');
       return;
     }
 
-    if (!validator.isEmail(formData.emailContato)) {
+    if (!validator.isEmail(emailContato)) {
       alert('Email do Contato inválido');
       return;
     }
 
-     axios.post('http://localhost:9090/clientes', formData)
-       .then((response) => {
-         console.log('Cliente cadastrado com sucesso', response.data);
-       })
-       .catch((error) => {
+    // Envie o CNPJ formatado com barras e traços (formData.cnpj) ou o CNPJ limpo (cnpjLimpo) conforme necessário
+    axios.post('http://localhost:9090/clientes', { ...formData, cnpj: cnpjLimpo })
+      .then((response) => {
+        console.log('Cliente cadastrado com sucesso', response.data);
+      })
+      .catch((error) => {
         console.error('Erro ao cadastrar cliente', error);
-       });
+      });
   };
 
   return (
@@ -69,8 +109,8 @@ function CriarCliente() {
             name="cnpj"
             value={formData.cnpj}
             onChange={handleChange}
-            mask="99.999.999/9999-99"
-            maskChar="_"
+            mask="99.999.999/9999-99" // Mantenha o mask para exibição formatada
+            maskChar="" // Remova o maskChar para aceitar apenas dígitos
           />
         </label>
         <label>
@@ -90,7 +130,7 @@ function CriarCliente() {
             value={formData.mcc}
             onChange={handleChange}
             mask="9999"
-            maskChar="_"
+            maskChar=""
           />
         </label>
         <label>
@@ -101,7 +141,7 @@ function CriarCliente() {
             value={formData.cpfContato}
             onChange={handleChange}
             mask="999.999.999-99"
-            maskChar="_"
+            maskChar=""
           />
         </label>
         <label>
